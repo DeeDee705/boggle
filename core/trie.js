e68e9e38bf9ce239f2634d6ead60c2a2cbb36d61
DeeDee705 <dieterdejonckheere@gmail.com>
2025-09-28 16:24:02 +0200

    Update trie.js

--- core/trie.js
+++ core/trie.js
@@ -1,1 +1,8 @@
+// core/trie.js
+export class Trie {
+  constructor(){ this.root = {}; }
+  insert(w){ let n=this.root; for (const ch of w) n = n[ch] ??= {}; n.$ = true; }
+  hasWord(w){ let n=this.root; for (const ch of w){ n=n[ch]; if(!n) return false; } return !!n.$; }
+  hasPrefix(p){ let n=this.root; for (const ch of p){ n=n[ch]; if(!n) return false; } return true; }
+}
 
