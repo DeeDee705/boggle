f35c481bbcdb35c5ccc041d8a1554c8e17165230
DeeDee705 <dieterdejonckheere@gmail.com>
2025-09-28 11:45:51 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ const GRID = {
   rows: 5, cols: 5,
   size: 38, gutter: 7,
   // raise grid higher (was 254.0)
-  topLeftCenter: { x: 72.366, y: 50,0 }
+  topLeftCenter: { x: 72.366, y: 25.0 }
 };
 
 const TIMER_R   = 26;
